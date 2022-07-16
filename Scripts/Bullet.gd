c42a8bfca0f28c34a372903d36986ba8a6a7a268
base6man <keatonmitchell000@gmail.com>
2022-07-16 13:04:02 -0500

    Added bullets

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,20 +1,16 @@
 extends RigidBody2D
 
 
-# Declare member variables here. Examples:
-var speed = 8
+var speed = 120
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass 
 	
-	pass # Replace with function body.
+	apply_central_impulse(Vector2(0, -speed))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-func _input(event):
-	if event.is_action_pressed("jump"):
-		# Do nothing
---
+++ Scripts/playerControl.gd
@@ -0,0 +1,72 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+var speed = 80
+var myFriction = 4
+var velocity = 0
+
+var leftInput = 0
+var rightInput = 0
+var upInput = 0
+var downInput = 0
+
+var bullet = preload("res://Bullets/Bullet.tscn")
+var bulletShootTime = 0
+var bulletCooldownLevel = 0
+
+var runtime = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	runtime += delta
+	
+	var idealSpeed = Vector2(rightInput - leftInput, downInput - upInput)
+	idealSpeed = idealSpeed.normalized() * speed
+	var currentSpeed = get_linear_velocity()
+	var totalFriction = min(myFriction * delta, 1)
+	var force = totalFriction * (idealSpeed - currentSpeed)
+	var newVelocity = get_linear_velocity() + force
+	
+	set_linear_velocity(newVelocity)
+	
+	pass
+
+func spawnBullet(_keyPressed):
+	if runtime - bulletShootTime < bulletCooldown(): return
+	
+	var bulletInstance = bullet.instance()
+	add_child(bulletInstance)
+	bulletShootTime = runtime
+
+func bulletCooldown():
+	return 0.5 * (10 - bulletCooldownLevel) / 10
+
+func _input(event):
+	if event.is_action_pressed("ui_left"):
+		leftInput = 1
+	if event.is_action_pressed("ui_right"):
+		rightInput = 1
+	if event.is_action_pressed("ui_up"):
+		upInput = 1
+	if event.is_action_pressed("ui_down"):
+		downInput = 1
+		
+	if event.is_action_released("ui_left"):
+		leftInput = 0
+	if event.is_action_released("ui_right"):
+		rightInput = 0
+	if event.is_action_released("ui_up"):
+		upInput = 0
+	if event.is_action_released("ui_down"):
+		downInput = 0
+	
+	if event.is_action_pressed("ui_accept"):
+		spawnBullet("J")
+	if event.is_action_pressed("ui_cancel"):
+		spawnBullet("K")
